1f4b9c7eb0192e7ae3eb27e4c8353f9012bed7e1
aswini3248 <113409861+aswini3248@users.noreply.github.com>
2023-10-04 12:52:02 +0530

    Create Duplicate elements.java

---
+++ Duplicate elements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class maxmin{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the array size: ");
+        int n=input.nextInt();
+        int i,j;
+        int a[]=new int[n];
+        System.out.println("Enter the array elements: ");
+        for(i=0;i<n;i++){
+            a[i]=input.nextInt();
+        }
+        System.out.println("Duplicate elements are: ");
+        for(i=0;i<n;i++){
+            for(j=i+1;j<n;j++){
+                if(a[i]==a[j]){
+                    System.out.println(a[j]);
+                }
+            }
+        }
+    }
+}
