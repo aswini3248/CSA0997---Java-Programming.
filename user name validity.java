7e14ee7858f58e4cebc7c149a53fc302a53473f2
aswini3248 <113409861+aswini3248@users.noreply.github.com>
2023-10-06 12:18:11 +0530

    Create user name validity.java

---
+++ user name validity.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class revstr{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter the user name: ");
+        String a=input.nextLine();
+        System.out.print("Reenter the user name: ");
+        String b=input.nextLine();
+        if(a==b){
+            System.out.print("User name is valid");
+        }
+        else{
+            System.out.print("User name is Invalid");
+        }
+    }
+}
